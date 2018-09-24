18e5f28eeb5b7e014cd7259b0e40a1c579715655
eogladkih <eogladkih@hotmail.com>
2018-09-24 12:17:46 -0700

    fix terraform

--- gitlab-ci/infra/check_statement.sh
+++ gitlab-ci/infra/check_statement.sh
@@ -3,7 +3,7 @@ STATE="$(./gce.py | grep $CI_COMMIT_REF_NAME)"
 if [ -z "$STATE" ]; then
    pwd
    terraform init
-   terraform apply -var instance_name=$CI_COMMIT_REF_NAME -auto-approve exit 0
+   terraform apply -var instance_name=$CI_COMMIT_REF_NAME -auto-approve
    sleep 300
    ansible-playbook main.yml   echo "empty"
 else 
