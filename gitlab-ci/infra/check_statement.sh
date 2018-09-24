f65af1e71e538170b812a931e0146254f23d1b11
eogladkih <eogladkih@hotmail.com>
2018-09-24 11:59:06 -0700

    add script for check statements

---
+++ gitlab-ci/infra/check_statement.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+STATE="$(./gce.py | grep $CI_COMMIT_REF_NAME)"
+if [ -z "$STATE" ]; then
+   terraform init
+   terraform apply -var instance_name=$CI_COMMIT_REF_NAME -auto-approve exit 0
+   sleep 300
+   ansible-playbook main.yml   echo "empty"
+else 
+   echo "not empty"
+fi  
