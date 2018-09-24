82e8006593ca76924d1e429cbc3c8759d7048809
eogladkih <eogladkih@hotmail.com>
2018-09-24 12:11:50 -0700

    pwd

--- gitlab-ci/infra/check_statement.sh
+++ gitlab-ci/infra/check_statement.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 STATE="$(./gce.py | grep $CI_COMMIT_REF_NAME)"
 if [ -z "$STATE" ]; then
+   pwd
    terraform init
    terraform apply -var instance_name=$CI_COMMIT_REF_NAME -auto-approve exit 0
    sleep 300
